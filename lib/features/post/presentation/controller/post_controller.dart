e839440052f8f901773bf2b3eeca8f86b064cbee
Nguyen The Hoang <hoang.nt1@3si.vn>
2023-10-19 16:12:05 +0700

    fix

--- lib/features/post/presentation/controller/post_controller.dart
+++ lib/features/post/presentation/controller/post_controller.dart
@@ -1,3 +1,44 @@
 import 'package:clean_pattern/common/controller/base_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_painter/flutter_painter.dart';
 
-class PostController extends BaseController {}
+class PostController extends BaseController {
+  PainterController paintController = PainterController();
+  FocusNode textFocusNode = FocusNode();
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    Paint shapePaint = Paint()
+      ..strokeWidth = 5
+      ..color = Colors.red
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round;
+
+    paintController = PainterController(
+      settings: PainterSettings(
+        text: TextSettings(
+          focusNode: textFocusNode,
+          textStyle: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.red,
+            fontSize: 18,
+          ),
+        ),
+        freeStyle: const FreeStyleSettings(
+          color: Colors.red,
+          strokeWidth: 5,
+        ),
+        shape: ShapeSettings(
+          paint: shapePaint,
+        ),
+        scale: const ScaleSettings(
+          enabled: true,
+          minScale: 1,
+          maxScale: 5,
+        ),
+      ),
+    );
+  }
+}
--- lib/features/post/presentation/pages/post_screen.dart
+++ lib/features/post/presentation/pages/post_screen.dart
@@ -1,6 +1,7 @@
 import 'package:clean_pattern/common/constant/app_color.dart';
 import 'package:clean_pattern/features/post/presentation/controller/post_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_painter/flutter_painter.dart';
 import 'package:get/get.dart';
 
 class PostScreen extends StatelessWidget {
@@ -12,14 +13,30 @@ class PostScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Share your Ramen"),
       ),
-      body: Container(
-        width: double.infinity,
-        color: AppColor.primary,
-        child: Column(
-          children: [
-            Text("do it"),
-          ],
-        ),
+      body: FlutterPainter.builder(
+        controller: _controller.paintController,
+        builder: (context, painter) {
+          return Container(
+            width: double.infinity,
+            color: AppColor.primary,
+            child: Stack(
+              children: [
+                painter,
+                ElevatedButton(
+                  onPressed: () {
+                    if (_controller.paintController.freeStyleMode !=
+                        FreeStyleMode.none) {
+                      _controller.paintController.freeStyleMode =
+                          FreeStyleMode.none;
+                    }
+                    _controller.paintController.addText();
+                  },
+                  child: Text("add text"),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
