a6d0b9066818c5b114cec6702b68e64eab558b96
Nguyen The Hoang <hoang.nt1@3si.vn>
2023-10-23 13:56:53 +0700

    fix

--- lib/common/controller/base_controller.dart
+++ lib/common/controller/base_controller.dart
@@ -17,11 +17,40 @@ class BaseController extends GetxController {
     final res = await usecase;
 
     if (res.statusCode != StatusCode.success && onError != null) {
-      onError(res.message ?? "Something wen wrong !");
+      onError(res.message ?? "Something went wrong !");
     }
 
     if (onSuccess != null) {
       onSuccess(res.data as T);
+    }
+
+    isLoading = false;
+    EasyLoading.dismiss();
+    update();
+  }
+
+  Future<void> handleMutiUsecase({
+    required List<Future<BaseResponse>> usecases,
+    String? messageLoading,
+    void Function()? onSuccess,
+    void Function(List<String>)? onError,
+  }) async {
+    isLoading = true;
+    EasyLoading.show(status: "Loading...");
+
+    final res = await Future.wait(usecases);
+
+    if (onError != null &&
+        res.any((val) => val.statusCode != StatusCode.success)) {
+      List<String> listMessageError = res
+          .where((val) => val.statusCode != StatusCode.success)
+          .map((val) => val.message ?? "Something went wrong !")
+          .toList();
+      onError(listMessageError);
+    }
+
+    if (onSuccess != null) {
+      onSuccess();
     }
 
     isLoading = false;
--- lib/features/home/presentation/pages/home_screen.dart
+++ lib/features/home/presentation/pages/home_screen.dart
@@ -30,11 +30,19 @@ class _HomeScreenState extends State<HomeScreen>
               return SizedBox();
             }
 
-            return ListView.builder(
-              itemCount: _controller.listArticle.length,
-              itemBuilder: (context, index) {
-                return itemArticle(_controller.listArticle[index]);
+            return RefreshIndicator(
+              onRefresh: () async {
+                _controller.handleBaseResponse<List<ArticleModel>>(
+                  usecase: _controller.getAllArticleUsecase(),
+                  onSuccess: (data) => _controller.listArticle = data,
+                );
               },
+              child: ListView.builder(
+                itemCount: _controller.listArticle.length,
+                itemBuilder: (context, index) {
+                  return itemArticle(_controller.listArticle[index], index);
+                },
+              ),
             );
           },
         ),
@@ -42,71 +50,78 @@ class _HomeScreenState extends State<HomeScreen>
     );
   }
 
-  Widget itemArticle(ArticleModel item) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 5.h),
-      margin: EdgeInsets.only(bottom: 20.h),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border.all(color: Color(0xffFDFDFD)),
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            offset: Offset(3, 3),
-            spreadRadius: 1.5,
-            blurRadius: 4,
-            color: Colors.black.withOpacity(0.2),
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            item.title!,
-            style: TextStyle(
-              letterSpacing: 1,
-              fontSize: 13.sp,
-              fontWeight: FontWeight.bold,
+  Widget itemArticle(ArticleModel item, int index) {
+    return Dismissible(
+      key: UniqueKey(),
+      onDismissed: (_) {
+        _controller.listArticle.removeAt(index);
+        _controller.update();
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 5.h),
+        margin: EdgeInsets.only(bottom: 20.h),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Color(0xffFDFDFD)),
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(3, 3),
+              spreadRadius: 1.5,
+              blurRadius: 4,
+              color: Colors.black.withOpacity(0.2),
             ),
-          ),
-          SizedBox(height: 5.h),
-          Text("Author: ${item.author!}"),
-          SizedBox(height: 2.h),
-          Text(
-            "Pubished: ${item.publishedAt!.toDDMMYYYYString()}",
-            style: TextStyle(
-              fontStyle: FontStyle.italic,
-              color: Colors.black.withOpacity(0.6),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              item.title!,
+              style: TextStyle(
+                letterSpacing: 1,
+                fontSize: 13.sp,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          SizedBox(height: 2.h),
-          Text(
-            item.content!,
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-              color: Colors.black.withOpacity(0.6),
+            SizedBox(height: 5.h),
+            Text("Author: ${item.author!}"),
+            SizedBox(height: 2.h),
+            Text(
+              "Pubished: ${item.publishedAt!.toDDMMYYYYString()}",
+              style: TextStyle(
+                fontStyle: FontStyle.italic,
+                color: Colors.black.withOpacity(0.6),
+              ),
             ),
-          ),
-          CachedNetworkImage(
-            imageUrl: item.image!,
-            errorWidget: (context, _, __) => Icon(Icons.error),
-            imageBuilder: (context, imageProvider) => Container(
-              height: 100.h,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(10),
-                  bottomLeft: Radius.circular(10),
-                ),
-                image: DecorationImage(
-                  image: imageProvider,
-                  fit: BoxFit.cover,
+            SizedBox(height: 2.h),
+            Text(
+              item.content!,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                color: Colors.black.withOpacity(0.6),
+              ),
+            ),
+            CachedNetworkImage(
+              imageUrl: item.image!,
+              errorWidget: (context, _, __) => Icon(Icons.error),
+              imageBuilder: (context, imageProvider) => Container(
+                height: 100.h,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    bottomLeft: Radius.circular(10),
+                  ),
+                  image: DecorationImage(
+                    image: imageProvider,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
